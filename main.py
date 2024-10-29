ad649ad9a473f3267646167f0624aa303f828bb1
Michael <michaelotengadjei@gmail.com>
2024-10-28 20:37:34 -0700

    update main.py

--- main.py
+++ main.py
@@ -14,6 +14,5 @@ if __name__ == '__main__':
     print_hi('Michael')
     print_hi('Pull Changes')
     print_hi('Merge from remote repo side')
-
     print_hi('Merge local repo end')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
